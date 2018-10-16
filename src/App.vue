59c72222e218c0494c27b937afab77d0efea47c9
sardorml <33747403+sardorml@users.noreply.github.com>
2018-10-16 18:38:53 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <mainPage/>
+  </div>
+</template>
+
+<script>
+import mainPage from "./components/main";
+
+export default {
+  name: "App",
+  components: {
+    mainPage
+  }
+};
+</script>
+
+<style>
+
+</style>
